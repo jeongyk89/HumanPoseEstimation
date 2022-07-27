9772669be0b6e1aef383b9e33d414356a356515c
Jesper <Yaglis@users.noreply.github.com>
2022-07-27 11:44:02 +0200

    First test

---
+++ Angle Heuristics.py
@@ -0,0 +1,189 @@
+"""Import Stuff"""
+import cv2
+import mediapipe as mp
+import math
+import numpy as np
+from time import time
+
+"""Set up mediapipe"""
+mp_drawing = mp.solutions.drawing_utils     # Drawing helpers
+mp_pose = mp.solutions.pose                 # Mediapipe Solutions
+
+cap = cv2.VideoCapture(0)
+time1 = 0
+
+
+# Finding the angle between 3 points
+def calculate_angle(a, b, c):
+    #print(a)
+    x1, y1, z1 = a
+    x2, y2, z2 = b
+    x3, y3, z3 = c
+    # Calculate the angle between the three points
+    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+    # Check if the angle is less than zero.
+    if angle < 0:
+        # Add 360 to the found angle.
+        angle += 360
+    # Return the calculated angle.
+    return angle
+
+
+    # a = np.array(a)  # Startpoint
+    # b = np.array(b)  # Midpoint
+    # c = np.array(c)  # Endpoint
+    # radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    # angle = np.abs(radians * 180.0/np.pi)
+    # angle = np.round(angle, 2)
+    # if angle.all() < 0:
+    #     angle += 360.0
+    # return angle
+
+
+def classify_pose(landmarks, image, display = False):
+    label = "Unknown pose"
+    color = (0, 0, 255)  # Red
+
+
+    """Angles for six landmarks"""
+    left_elbow_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
+                                        landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
+                                        landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value]
+                                        )
+
+    right_elbow_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                         landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value],
+                                         landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value]
+                                         )
+
+    left_shoulder_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.LEFT_HIP.value],
+                                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
+                                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value]
+                                         )
+
+    right_shoulder_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
+                                        landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                        landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value]
+                                        )
+
+    left_knee_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.LEFT_HIP.value],
+                                        landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value],
+                                        landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value]
+                                        )
+
+    right_knee_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
+                                        landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value],
+                                        landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value]
+                                        )
+
+    # # Check is both arms are straight
+    # if (left_elbow_angle > 165 and left_elbow_angle < 195) and (right_elbow_angle > 165 and right_elbow_angle < 195):
+    #     # Check if both shoulders are perpendicular to the body
+    #     if (left_shoulder_angle > 80 and left_shoulder_angle < 110) and (right_shoulder_angle > 80 and right_elbow_angle < 110):
+    #
+    # # Check if Warrior II Pose
+    #         # Check if one leg is straight
+    #         if (left_knee_angle > 165 and left_knee_angle < 195) or (right_knee_angle > 165 and right_knee_angle < 195):
+    #             # Check if other knee is perpendicular
+    #             if (left_knee_angle > 80 and left_knee_angle < 110) or (right_knee_angle > 80 and right_knee_angle < 110):
+    #
+    #                 label = "Warrior II Pose"
+
+    # Check if a shoulder is straight
+    if (right_shoulder_angle > 70 and right_shoulder_angle < 110):# or (left_shoulder_angle > 70 and left_shoulder_angle < 110):
+        # Check if elbow is perpendicular
+        print('a')
+        if (right_elbow_angle > 70 and right_elbow_angle < 110):# or (left_elbow_angle > 70 and left_elbow_angle < 110):
+            print('b')
+            label = "Arm up"
+
+
+    if label is not "Uknown pose":
+        color = (0, 255, 0)  # Green
+
+    cv2.putText(image, label, (10, 60), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 4)
+    cv2.putText(image, label, (10, 60), cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+
+    return image, label
+
+
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        success, frame = cap.read()
+
+        if not success:  # Sometimes I had to force quit the program, this prevents that. Sort of.
+            break
+
+        # Flip the video to be a "mirror" on the laptop.
+        frame = cv2.flip(frame, 1)
+
+        # Convert from BGR to RGB colorspace - MediaPipe likes RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        """Detect the poses"""
+        # Make Detections
+        results = pose.process(image)
+
+        # Recolor image back to BGR for rendering - opencv likes BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Avoid breaking the loop is no frame is extracted
+        try:
+            landmarks = results.pose_landmarks.landmark
+        except:
+            pass
+
+        # 1. Right hand
+        # mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_pose.HAND_CONNECTIONS,
+        #                           mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+        #                           mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+        #                           )
+        #
+        # # 2. Left Hand
+        # mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_pose.HAND_CONNECTIONS,
+        #                           mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+        #                           mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+        #                           )
+
+        # 3. Pose Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
+
+        time2 = time()
+        if (time2 - time1) > 0:
+            fps = 1/(time2 - time1)
+            # FPS counter in green with a black border to make it easy to read
+            cv2.putText(image, 'FPS: {}'.format(int(fps)), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 6)
+            cv2.putText(image, 'FPS: {}'.format(int(fps)), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
+        time1 = time2
+
+        display = False
+
+        # Unpack coordinates and put them in a list
+        height, width, depth = image.shape
+        landmarks = []
+        if len(landmarks) is 0:
+            for landmark in results.pose_landmarks.landmark:
+                # Append the landmark into the list.
+                landmarks.append((int(landmark.x * width),
+                                  int(landmark.y * height),
+                                  int((landmark.z * depth)))
+                                 )
+
+        image, label = classify_pose(landmarks, image, display)
+
+        cv2.imshow('Webcam is running :)', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# print(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value])
+
